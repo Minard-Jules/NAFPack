05bdcef6ae11f4084fc35afa8dbecba9f0f40908
Jules Minard <minard.jules@yahoo.com>
2026-08-08 15:01:28 +0200

    fix(cmake): update CMakeLists.txt and FlagsConfig.cmake to fix build

--- config/FlagsConfig.cmake
+++ config/FlagsConfig.cmake
@@ -6,9 +6,14 @@ if(CMAKE_Fortran_COMPILER_ID STREQUAL "GNU")
     )
     set(
         CMAKE_Fortran_FLAGS_RELEASE_INIT
-        "-O2"
+        "-O3"
+        "-march=native"
         "-fimplicit-none"
         "-ffree-line-length-2000"
+        "-funroll-loops"
+        "-ffast-math"
+
+        # "-flto"
     )
     set(
         CMAKE_Fortran_FLAGS_DEBUG_INIT
@@ -20,11 +25,14 @@ if(CMAKE_Fortran_COMPILER_ID STREQUAL "GNU")
         "-std=f2018"
         "-g"
         "-fbacktrace"
+        "-fbounds-check"
+        "-frange-check"
     )
 elseif(CMAKE_Fortran_COMPILER_ID STREQUAL "Intel")
 elseif(CMAKE_Fortran_COMPILER_ID STREQUAL "Flang")
 elseif(CMAKE_Fortran_COMPILER_ID STREQUAL "LFortran")
 endif()
+
 string(REPLACE ";" " " CMAKE_Fortran_FLAGS_INIT "${CMAKE_Fortran_FLAGS_INIT}")
 string(REPLACE ";" " " CMAKE_Fortran_FLAGS_RELEASE_INIT "${CMAKE_Fortran_FLAGS_RELEASE_INIT}")
 string(REPLACE ";" " " CMAKE_Fortran_FLAGS_DEBUG_INIT "${CMAKE_Fortran_FLAGS_DEBUG_INIT}")
